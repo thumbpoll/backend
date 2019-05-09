6c6ba9ba148a387eefc2c1b40104f7db948432f5
Dicky Muhamad R <dickymr.dev@gmail.com>
2019-05-09 15:57:45 +0700

    Added config.js

---
+++ config/config.js
@@ -0,0 +1,11 @@
+require('dotenv').config()
+const mongoose = require('mongoose')
+const MONGODB_CONNECTION = process.env.MONGODB_URI || 'mongodb://localhost:27017/config'
+
+mongoose.set('useCreateIndex', true)
+
+mongoose.connect(MONGODB_CONNECTION, {
+  useNewUrlParser: true
+})
+
+module.exports = mongoose
