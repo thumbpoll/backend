e58a9eb503219a000c8c7e5ad07b855a60f254f5
dickymr <dickymr.dev@gmail.com>
2019-05-14 18:49:41 +0700

    uncomment router post

--- routes/optionsRouter.js
+++ routes/optionsRouter.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const Controller = require("../controllers/options");
 const Helpers = require("../helpers/index");
 
-// router.post("/", Helpers.isAuthenticated, Controller.createNewOption);
+router.post("/", Controller.createNewOption);
 // router.put("/:id", Helpers.isAuthenticated, Controller.updateOption);
 // router.delete("/:id", Helpers.isAuthenticated, Controller.deleteOneOption);
 // router.get("/", Controller.getAllOption);
