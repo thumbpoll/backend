db9f08f47edd376ece7ec7d9aca0352395272d94
Muhammad Riza <riza.riza69@gmail.com>
2019-05-15 13:08:13 +0700

    improve options router

--- routes/optionsRouter.js
+++ routes/optionsRouter.js
@@ -2,9 +2,7 @@ const router = require("express").Router();
 const Controller = require("../controllers/options");
 const Helpers = require("../helpers/index");
 
-router.post("/", Controller.createNewOption);
-// router.put("/:id", Helpers.isAuthenticated, Controller.updateOption);
-// router.delete("/:id", Helpers.isAuthenticated, Controller.deleteOneOption);
+router.post("/", Helpers.isAuthenticated, Controller.createNewOption);
 router.get("/", Controller.getAllOption);
 
 module.exports = router;
